c33e00771dbcf1b32a3ea0f408ca79f7061f7000
Rizwan Khot <khotrizwan@gmail.com>
2021-06-21 01:59:25 +0530

    Angular Two-Way Data Binding Example done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-two-way-binding';
+  title = 'Angular Two-Way Data Binding Example';
+  count = 0;
+  startCount: string = "0";
+  endCount: string = "10";
+  allowInput = false;
+  constructor() { 
+  }
+
+  ngOnInit() {
+    console.log(this.title);
+    
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        this.countFn(this.count + 1)
+      }, 1000);
+    
+  }
+
+  countFn (count: any): any {
+    this.count = count;
+    console.log(count);
+    if(count < Number(this.endCount)) {
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        this.countFn(this.count + 1)
+      }, 1000);
+    } else {
+      this.allowInput = true;
+    }
+  }
+
+  restart() {
+    this.allowInput = false;
+    this.count = Number(this.startCount);
+    setTimeout(()=>{                           //<<<---using ()=> syntax
+      this.countFn(this.count + 1)
+    }, 1000);
+  }
+  reset() {
+    this.startCount = "0";
+    this.endCount = "10";
+  }
 }
